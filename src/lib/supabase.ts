9decae89092014356e7218051b84103bcf9bb742
Gab <gabrieltc555@gmail.com>
2024-01-28 19:26:05 -0400

    add pkce flow for oAuth

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,4 +6,8 @@ dotenv.config();
 const uri = process.env.SUPABASE_URL as string;
 const anonKey = process.env.SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(uri, anonKey);
+export const supabase = createClient(uri, anonKey, {
+  auth: {
+    flowType: "pkce",
+  },
+});
