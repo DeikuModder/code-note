409b66a0f358a1ff51ac195857173caeab2bb7a4
Gab <gabrieltc555@gmail.com>
2024-01-28 19:25:35 -0400

    unify the list component for notes

---
+++ src/components/Notes/Display/ListNotes.tsx
@@ -0,0 +1,24 @@
+import type { Notes } from "@/src/types";
+import Note from "./Note";
+
+const ListNotes = ({
+  notes,
+  fallbackMessage,
+}: {
+  notes: Notes[];
+  fallbackMessage: string;
+}) => {
+  return (
+    <ul className="w-[90%] border border-neutral-700 rounded-lg p-4 flex flex-col gap-4 min-h-80 max-w-[500px]">
+      {notes.length <= 0 ? (
+        <p>{fallbackMessage}</p>
+      ) : (
+        notes.map((note, index) => {
+          return <Note note={note} key={index} />;
+        })
+      )}
+    </ul>
+  );
+};
+
+export default ListNotes;
