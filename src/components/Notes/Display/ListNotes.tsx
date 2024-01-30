0a571affb2b64dac995e03e3e8732de348246471
Gab <gabrieltc555@gmail.com>
2024-01-30 14:57:28 -0400

    center the fallback message

--- src/components/Notes/Display/ListNotes.tsx
+++ src/components/Notes/Display/ListNotes.tsx
@@ -11,7 +11,7 @@ const ListNotes = ({
   return (
     <ul className="w-[90%] border border-neutral-700 rounded-lg p-4 flex flex-col gap-4 min-h-80 max-w-[500px]">
       {notes.length <= 0 ? (
-        <p>{fallbackMessage}</p>
+        <p className="text-2xl w-full text-center">{fallbackMessage}</p>
       ) : (
         notes.map((note, index) => {
           return <Note note={note} key={index} />;
