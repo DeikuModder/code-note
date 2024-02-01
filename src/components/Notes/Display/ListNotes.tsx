05ad02fc11f04fda79680144c42f461e500fbec6
Gab <gabrieltc555@gmail.com>
2024-02-01 14:55:04 -0400

    add overflow and a max height to list

--- src/components/Notes/Display/ListNotes.tsx
+++ src/components/Notes/Display/ListNotes.tsx
@@ -9,7 +9,7 @@ const ListNotes = ({
   fallbackMessage: string;
 }) => {
   return (
-    <ul className="w-[90%] border border-neutral-700 rounded-lg p-4 flex flex-col gap-4 min-h-80 max-w-[500px]">
+    <ul className="w-[90%] border border-neutral-700 rounded-lg p-4 flex flex-col gap-4 min-h-80 max-w-[500px] max-h-[450px] overflow-auto">
       {notes.length <= 0 ? (
         <p className="text-2xl w-full text-center">{fallbackMessage}</p>
       ) : (
