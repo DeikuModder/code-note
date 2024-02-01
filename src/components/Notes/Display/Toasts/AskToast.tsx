b65553d6a12677ae3c1efe7ff12ee6d8365ea24c
Gab <gabrieltc555@gmail.com>
2024-02-01 15:00:41 -0400

    remove unused import

--- src/components/Notes/Display/Toasts/AskToast.tsx
+++ src/components/Notes/Display/Toasts/AskToast.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./styles/toast.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
