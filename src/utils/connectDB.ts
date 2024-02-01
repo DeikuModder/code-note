328dcb2bd4a5451e55e7b2c897140665dfee97e9
Gab <gabrieltc555@gmail.com>
2024-02-01 14:52:33 -0400

    add function to connect to mongodb

---
+++ src/utils/connectDB.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+export async function connectDB() {
+  dotenv.config();
+
+  const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@burguer-app.nj6tkpl.mongodb.net/?retryWrites=true&w=majority`;
+
+  async function connect() {
+    try {
+      await mongoose.connect(uri, {
+        dbName: "Notes",
+      });
+      console.log("Connected to Database!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  await connect();
+}
