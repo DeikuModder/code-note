1e6a00e80f2850fa9bd0ebcb7c18e360ce47651a
Gab <gabrieltc555@gmail.com>
2024-02-01 14:53:08 -0400

    add mongoose note schema

---
+++ src/schemas/Note.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    priority: { type: String, required: true },
+    status: { type: String, required: true },
+    userID: { type: String, required: true },
+    id: { type: String },
+    description: { type: String },
+    deadline: { type: Date },
+    documLinks: [
+      {
+        title: { type: String },
+        url: { type: String },
+      },
+    ],
+    codeSnippets: { type: [String] },
+    videos_info: [
+      {
+        srcLink: String,
+        title: String,
+      },
+    ],
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Note = model("Note", noteSchema);
+
+export default Note;
