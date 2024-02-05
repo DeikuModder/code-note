4bd9604a687b3a664e0b02293b1625cd1671ad2e
Gab <gabrieltc555@gmail.com>
2024-02-05 02:03:40 -0400

    store env variable in a const

--- src/lib/cohereClient.ts
+++ src/lib/cohereClient.ts
@@ -1,5 +1,7 @@
 import { CohereClient } from "cohere-ai";
 
+const token = import.meta.env.AI_API_KEY;
+
 export const cohere = new CohereClient({
-  token: import.meta.env.AI_API_KEY,
+  token: token,
 });
