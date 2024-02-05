b669be5ab96cb254e31a4add04b0d1c3ddbcb5a7
Gab <gabrieltc555@gmail.com>
2024-02-05 03:14:33 -0400

    change to command-light model, set maxrate at 150 words and randomness at 0.5

--- src/pages/api/notes/ai/generate.ts
+++ src/pages/api/notes/ai/generate.ts
@@ -1,15 +1,21 @@
 import { cohere } from "@/lib/cohereClient";
+import type { Generation } from "@/src/types";
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
   try {
     const data = (await request.json()) as { prompt: string };
 
-    const generate = await cohere.generate({
+    const generate = (await cohere.generate({
       prompt: data.prompt,
-    });
+      model: "command-light",
+      maxTokens: 150,
+      temperature: 0.5,
+    })) as Generation;
 
-    return new Response(JSON.stringify(generate), { status: 200 });
+    return new Response(JSON.stringify(generate.generations[0].text), {
+      status: 200,
+    });
   } catch (error) {
     return new Response(JSON.stringify({ error: "Couldn't get response" }), {
       status: 500,
