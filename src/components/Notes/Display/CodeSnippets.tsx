6720211db21ca472050ded0ff44bcdf4e67d3e2a
Gab <gabrieltc555@gmail.com>
2024-04-08 17:20:50 -0400

    update code snippets font

--- src/components/Notes/Display/CodeSnippets.tsx
+++ src/components/Notes/Display/CodeSnippets.tsx
@@ -2,7 +2,7 @@ import CopyToClipboard from "../CopyToClipboard";
 
 const CodeSnippets = ({ code }: { code: string }) => {
   return (
-    <div className="bg-cyan-950 text-gray-200 w-full h-52 p-4 overflow-auto">
+    <div className="bg-cyan-950 text-gray-200 w-full h-52 p-4 overflow-auto font-mono">
       <div className="w-full flex justify-end">
         <CopyToClipboard content={code} />
       </div>
